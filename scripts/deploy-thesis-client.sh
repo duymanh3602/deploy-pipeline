681c74820cabf24b2a20934ec9f16bda2688b97a
duymanh3602 <82275607+duymanh3602@users.noreply.github.com>
2023-12-20 15:51:29 +0700

    test: deploy

---
+++ scripts/deploy-thesis-client.sh
@@ -0,0 +1,22 @@
+// Xóa thư mục clone cũ
+rm -r my-thesis
+
+// Clone code mới
+git clone git@github.com:duymanh3602/my-thesis.git
+
+wait
+// di chuyển đến client
+cd ./my-thesis/client
+
+// cài thư viện
+yarn
+
+wait
+// 
+cp -r ./dist /var/www
+
+cd /var/www
+
+rm -r ./html
+
+mv ./dist /html
